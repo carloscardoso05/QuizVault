79afe0299f89cac48e8d7ea71817ebb0011024af
Carlos Cardoso <cvcardos5@gmail.com>
2023-11-07 20:42:32 -0300

    Commit inicial

---
+++ src/main/java/com/QuizzParty/QuizVault/QuizVaultApplication.java
@@ -0,0 +1,13 @@
+package com.QuizzParty.QuizVault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizVaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/QuizzParty/QuizVault/QuizVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.QuizzParty.QuizVault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
